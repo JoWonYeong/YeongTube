04e153bc7aea95c0950848f55a305ae7853780e2
JoWonYeong <wswy17@gmail.com>
2023-08-19 14:35:25 +0900

    :seedling: Init: YeongTube 초기세팅, 목업데이터, 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import SearchHeader from './components/SearchHeader'
+import Videos from "./pages/Videos";
+import VideoDetail from './pages/VideoDetail';
+import NotFound from "./pages/NotFound";
+
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <SearchHeader />
+      <Routes>
+        <Route path='/' element={<Navigate to='/videos' replace />} />
+        <Route path='/videos' element={<Videos />} />
+        <Route path='/videos/:keyword' element={<Videos />} />
+        <Route path='/videos/watch/:videoId' element={<VideoDetail />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 export default App;
---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SearchHeader() {
+  return (
+    <h2>
+      SearchHeader
+    </h2>
+  );
+}
+
