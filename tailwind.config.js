7489c204d143bf92fb3364c352be15ba10313721
JoWonYeong <wswy17@gmail.com>
2023-08-23 21:03:07 +0900

    :teddy_bear: Chore: Tailwind 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors:{
+        'logo-red':'#ff0000'
+      }
+    },
+  },
+  plugins: [],
+};
+
